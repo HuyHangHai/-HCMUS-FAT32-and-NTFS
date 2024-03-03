96f6029bcd2b1a460d4e608566b053f8e30e3491
Khang <khang gay>
2024-03-03 21:43:51 +0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Fat32.py

--- Fat32.py
+++ Fat32.py
@@ -1,4 +1,5 @@
-BOOTSECTORSIZE = 512 
+BOOTSECTORSIZE = 512
+
 
 class Fat32:
     def __init__(self, volume_name) -> None:
@@ -6,12 +7,12 @@ class Fat32:
         try:
             self.bin_raw_data = open(rf"\\.\{self.volume_name}", 'rb')
             self.boot_sector = {}
-            
+
             self.boot_sector_data = self.bin_raw_data.read(BOOTSECTORSIZE)
             self.extract_boot_sector()
             if self.boot_sector['FAT Name'] != b'FAT32   ':
                 raise Exception('NOT FAT32')
-            
+
             # Important Info
             self.boot_sector['FAT Name'] = self.boot_sector['FAT Name'].decode()
             self.BS = self.boot_sector['Bytes Per Sector']
@@ -25,6 +26,29 @@ class Fat32:
 
         except Exception as error:
             print(f"Error: {error}")
+
+    @staticmethod
+    def check(volume_name):
+        try:
+            boot_sector = open(rf'\\.\{volume_name}', 'rb')
+            boot_sector.read(1)  # Ensure file pointer correctly point to boot sector
+            boot_sector.seek(0x52)
+            fat_type = boot_sector.read(8)
+            if fat_type == b'FAT32   ':
+                return True
+            return False
+        except Exception as error:
+            print(f'Error: {error}')
+            exit()
+
+    def __str__(self) -> s
+        tr:
+        s = "Volume's name: " + self.volume_name
+        s += "\nVolume's info:\n"
+        items = self.boot_sector.items()
+        for i in items:
+            s += str(i[0]) + ': ' + str(i[1]) + '\n'
+        return s
 
     def extract_boot_sector(self):
         self.boot_sector['Bytes Per Sector'] = int.from_bytes(self.boot_sector_data[0xB:0xD], 'little')
@@ -42,21 +66,11 @@ class Fat32:
         # self.boot_sector['Sector Storing Sub-Info'] = self.boot_sector_data[0x30:0x32]
         # self.boot_sector['Sector Storing Backup Boot Sector'] = self.boot_sector_data[0x32:0x34]
         self.boot_sector['FAT Name'] = self.boot_sector_data[0x52:0x5A]
-        self.boot_sector['Starting Sector of Data'] = self.boot_sector['Sectors in Boot Sector'] + self.boot_sector['FAT Numbers'] * self.boot_sector['Sectors Per FAT']   
+        self.boot_sector['Starting Sector of Data'] = self.boot_sector['Sectors in Boot Sector'] + self.boot_sector[
+            'FAT Numbers'] * self.boot_sector['Sectors Per FAT']
 
 
 
-    @staticmethod
-    def check(volume_name):
-        try:
-            boot_sector = open(rf'\\.\{volume_name}', 'rb')
-            boot_sector.read(1)  # Ensure file pointer correctly point to boot sector
-            boot_sector.seek(0x52)
-            fat_type = boot_sector.read(8)
-            if fat_type == b'FAT32   ':
-                return True
-            return False
-        except Exception as error:
-             print(f'Error: {error}')
-             exit()
-            
+
+
+
