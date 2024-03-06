e2ca21d62ae9e945c61916d184d278798844def0
Khang <khang gay>
2024-03-06 10:53:19 +0700

    fixed all previous error (datetime related error and 1 entries error)

--- Fat32.py
+++ Fat32.py
@@ -51,6 +51,9 @@ class RDETentry:
                 return
 
             self.attr = Attribute(int.from_bytes(self.flag, byteorder='little'))
+            if Attribute.VOL_LABLE in self.attr:
+                self.is_label = True
+                return
             self.set_date_time()
             self.start_cluster, self.size = self.extract_start_cluster_size()
         else:
@@ -60,26 +63,28 @@ class RDETentry:
     def set_date_time(self):
         self.time_created_raw = int.from_bytes(self.raw_data[0xD:0x10], byteorder='little')
         self.date_created_raw = int.from_bytes(self.raw_data[0x10:0x12], byteorder='little')
-        self.last_accessed_raw = int.from_bytes(self.raw_data[0x12:0x14], byteorder='little')
+        self.date_last_accessed_raw = int.from_bytes(self.raw_data[0x12:0x14], byteorder='little')
         self.time_updated_raw = int.from_bytes(self.raw_data[0x16:0x18], byteorder='little')
         self.date_updated_raw = int.from_bytes(self.raw_data[0x18:0x1A], byteorder='little')
 
         self.date_created = self.extract_datetime(self.time_created_raw, self.date_created_raw)
-        self.last_accessed = self.extract_datetime(self.last_accessed_raw, None)
+        self.last_accessed = self.extract_datetime(None, self.date_last_accessed_raw)
         self.date_updated = self.extract_datetime(self.time_updated_raw, self.date_updated_raw)
 
     def extract_datetime(self, time_raw, date_raw):
         if time_raw is None:
             return None
+        hours = (time_raw & 0b111110000000000000000000) >> 19
+        minutes = (time_raw & 0b000001111110000000000000) >> 13
+        seconds = (time_raw & 0b000000000001111110000000) >> 7
+        ms = (self.time_created_raw & 0b000000000000000001111111)
+        print('date:', hours, ' ', minutes, ' ', seconds)
         year = 1980 + ((date_raw & 0b1111111000000000) >> 9)
         month = (date_raw & 0b0000000111100000) >> 5
         day = date_raw & 0b0000000000011111
+        print('daytime:', year, ' ', month, ' ', day)
         if date_raw is None:
             return datetime(year, month, day)
-
-        hours = (time_raw & 0b1111100000000000) >> 11
-        minutes = (time_raw & 0b0000011111100000) >> 5
-        seconds = (time_raw & 0b0000000000011111) * 2
         return datetime(year, month, day, hours, minutes, seconds)
 
     def extract_start_cluster_size(self):
@@ -111,7 +116,6 @@ class RDET:
     def __init__(self, data: bytes) -> None:
         self.raw_data = data
         self.entries: list[RDETentry] = []
-        self.entries.append(RDETentry(self.raw_data[0: 32]))
         self.entries = self.get_full_entry_name()
         
         print(self.entries[-1].entry_name)
@@ -132,11 +136,26 @@ class RDET:
             else:
                 extension = entries[-1].ext.strip().decode()
                 if extension != '':
-                    entries[-1].entry_name = entries[-1].name + '.' + extension
+                    entries[-1].entry_name = entries[-1].name.strip().decode() + '.' + extension
                 else:
-                    entries[-1].entry_name = entries[-1].name
-                entry_name = ''
+                    entries[-1].entry_name = entries[-1].name.strip().decode()
+            entry_name = ''
         return entries
+
+    def get_active_entries(self) -> 'list[RDETentry]':
+        entry_list = []
+        for i in range(len(self.entries)):
+            if self.entries[i].is_active_entry():
+                entry_list.append(self.entries[i])
+        return entry_list
+
+    def find_entry(self, name) -> RDETentry:
+        for i in range(len(self.entries)):
+            if self.entries[i].is_active_entry() and self.entries[i].long_name.lower() == name.lower():
+                return self.entries[i]
+        return None
+
+
 class Fat32_Main:
     def __init__(self, volume_name) -> None:
         self.volume_name = volume_name
@@ -162,7 +181,7 @@ class Fat32_Main:
             
             # Read FAT's info
             # Move cursor in file to the 1st FAT
-            self.bin_raw_data.read(self.bytes_per_sector * (self.sectors_in_boot_sectors - 1))
+            self.temp = self.bin_raw_data.read(self.bytes_per_sector * (self.sectors_in_boot_sectors - 1))
             FAT_size = self.bytes_per_sector * self.sectors_per_fats
 
             self.list_FAT: list[FAT] = []
@@ -172,7 +191,8 @@ class Fat32_Main:
             # Handle RDET 
             starting_cluster_index = self.boot_sector["Starting Cluster of RDET"]
             self.RDET = RDET(self.get_all_cluster_data(starting_cluster_index))
-
+            self.DET = {}
+            self.DET[starting_cluster_index] = self.RDET
         except Exception as error:
             print(f"Error: {error}")
             exit()
@@ -194,12 +214,11 @@ class Fat32_Main:
         self.boot_sector['Sectors Per FAT'] = int.from_bytes(self.boot_sector_data[0x24:0x28], 'little')
         self.boot_sector['Starting Cluster of RDET'] = int.from_bytes(self.boot_sector_data[0x2C:0x30], 'little')
         self.boot_sector['FAT Name'] = self.boot_sector_data[0x52:0x5A]
-        self.boot_sector['Starting Sector of Data'] = self.boot_sector['Reserved Sectors'] + self.boot_sector[
-            'Number of FATs'] * self.boot_sector['Sectors Per FAT']
+        self.boot_sector['Starting Sector of Data'] = self.boot_sector['Reserved Sectors'] + self.boot_sector['Number of FATs'] * self.boot_sector['Sectors Per FAT']
 
     def convert_cluster_to_sector_index(self, index):
         return self.sectors_in_boot_sectors + self.sectors_per_fats * self.numbers_of_fats + (index - 2) * self.sectors_per_cluster
-
+#return self.SB + self.SF * self.NF + (index - 2) * self.SC
     def get_all_cluster_data(self, cluster_index):
         cluster_list = self.list_FAT[0].get_cluster_chain(cluster_index)
         data = b""
